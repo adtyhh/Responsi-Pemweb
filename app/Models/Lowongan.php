967d50de8c2ba8c48a3443fff8e0342203915ef7
Aditya Hastungkara <kodetyk@gmail.com>
2025-07-05 16:07:05 +0700

    Implementasi Model, Controller, dan CRUD Lowongan

---
+++ app/Models/Lowongan.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lowongan extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'judul_pekerjaan',
+        'nama_perusahaan',
+        'lokasi',
+        'gaji',
+        'deskripsi_pekerjaan',
+        'logo_perusahaan',
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LowonganController; // Tambahkan ini
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the Application's ServiceProvider and all
+| of them will be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +27,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Route untuk Lowongan
+    Route::resource('lowongan', LowonganController::class); // Tambahkan ini
 });
 
 require __DIR__.'/auth.php';
